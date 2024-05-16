22a1fd1ef03dd467d2fee9a74fec575a3d0ddf42
= <=>
2024-05-16 15:20:22 +0700

    Change subject class

---
+++ src/main/java/com/lab/diemdanh/Entity/Category.java
@@ -0,0 +1,41 @@
+package com.lab.diemdanh.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
